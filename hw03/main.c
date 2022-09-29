590028047a00f8e8244a32e20fcba94f11a3c43c
pantom <tomas.bahnik@gmail.com>
2022-09-29 18:40:10 +0200

    hw03 load input(working)

--- hw03/main.c
+++ hw03/main.c
@@ -1,11 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define ERROR_INPUT 100
+enum
+{
+  BORDER_LEFT = -10000,
+  BORDER_RIGHT = 10000
+};
 
 /* The main program */
 int main(int argc, char *argv[])
 {
-  // TODO - insert your code here
-  
-  return 0;
+  int x;
+  int ret = EXIT_SUCCESS;
+  int sc;
+  while ((sc = scanf("%d", &x)) != EOF)
+  {
+    if (x < BORDER_LEFT || x > BORDER_RIGHT)
+    {
+      // sc = false;
+      printf("\n");
+      printf("Vstup je mimo interval!\n");
+      return ERROR_INPUT;
+    }
+    else
+    {
+      printf("%d", x);
+      if (sc != EOF) //while cyclus does not reach last value + 1 ---> that value giv us EOS
+        printf(", ");
+      else
+        printf("\n");
+    }
+  };
+
+  // printing rows
+  /* printf("Desitkova soustava: %d %d\n", x, y);
+  printf("Sestnactkova soustava: %x %x\n", x, y);
+  printf("Soucet: %d + %d = %d\n", x, y, x + y);
+  printf("Rozdil: %d - %d = %d\n", x, y, x - y);
+  printf("Soucin: %d * %d = %d\n", x, y, x * y);
+  if (y == 0) {
+    printf("Nedefinovany vysledek!\n");
+  } else
+    printf("Podil: %d / %d = %d\n", x, y, x / y);
+  printf("Prumer: %.1f\n", avg);
+   */
+
+  return ret;
 }
 
+int read_numbers(int x)
+{
+
+  return x;
+};
