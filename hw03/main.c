b07bbba73fcdd52c7a407e724aa20ff58fb2e588
pantom <tomas.bahnik@gmail.com>
2022-10-03 15:28:12 +0200

    FINALL_try brute

--- hw03/main.c
+++ hw03/main.c
@@ -13,17 +13,29 @@ enum
 };
 
 // functions
-int read_numbers(void);
+void print_output(int count, int even, int possitive, float avg, int max, int min);
 
 /* The main program */
 int main(int argc, char *argv[])
 {
   int x;
   int ret = EXIT_SUCCESS;
-  int count = 0;
-   while (scanf("%d", &x) != EOF)
+  int count, even, possitive, max = 0, min = 0;
+  float avg = 0;
+  while (scanf("%d", &x) != EOF)
   {
+    // counters
     count += 1;
+    avg += x;
+    if (x >= 0)
+      possitive += 1;
+    if (x % 2 == 0)
+      even += 1;
+    if (max < x)
+      max = x;
+    if (x < min)
+      min = x;
+
     // check if input is out of interval
     if (x < BORDER_LEFT || x > BORDER_RIGHT)
     {
@@ -38,8 +50,32 @@ int main(int argc, char *argv[])
     }
     printf("%d", x);
   };
-  printf("\n");
-  printf("pocet: %d\n", count);
-
+  print_output(count, even, possitive, avg, max, min);
   return ret;
+};
+
+// function that print outputs
+void print_output(int count, int even, int possitive, float avg, int max, int min)
+{
+  int negativ = count - possitive;
+  int odd = count - even;
+  float per_possitiv = (100.00 / count) * possitive;
+  float per_negativ = (100.00 / count) * negativ;
+  float per_even = (100.00 / count) * even;
+  float per_odd = (100.00 / count) * odd;
+  float avg_res = avg / count;
+ 
+  printf("\n");
+  printf("Pocet cisel: %d\n", count);
+  printf("Pocet kladnych: %d\n", possitive);
+  printf("Pocet zapornych: %d\n", negativ);
+  printf("Procento kladnych: %.2f\n", per_possitiv);
+  printf("Procento zapornych: %.2f\n", per_negativ);
+  printf("Pocet sudych: %d\n", even);
+  printf("Pocet lichych: %d\n", odd);
+  printf("Procento sudych: %.2f\n", per_even);
+  printf("Procento lichych: %.2f\n", per_odd);
+  printf("Prumer: %.2f\n", avg_res);
+  printf("Maximum: %d\n", max);
+  printf("Minimum: %d\n", min);
 };
