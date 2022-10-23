20c2182ff295606d77f5b25339183657d19c581e
pantom <tomas.bahnik@gmail.com>
2022-10-23 21:03:30 +0200

    add reading first number

--- hw06/main.c
+++ hw06/main.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INT_STR_LEN 10
 
 // [a-z_A-Z]
-char *read_input_message(int *str_len);
+char *read_input_message(int *str_len, int *capacity);
 void rotate(char original[], int len_org);
 void compare(char str[], char str_enc[], int str_enc_len);
 void print_error(int error);
 
-enum { ERROR_INPUT = 100, ERROR_LENGHT = 101 };
+enum
+{
+    ERROR_INPUT = 100,
+    ERROR_LENGHT = 101
+};
 const char *const error_str_input = "Error: Chybny vstup!";
 const char *const error_str_lenght = "Error: Chybna delka vstupu!";
 
@@ -23,17 +26,30 @@ int main(int argc, char *argv[])
 
     str_enc = str = str_rot = NULL;
     str_enc_len = str_len = str_tmp_len = 0;
-
-    str_enc = read_input_message(&str_enc_len); // read first row
-    if (str_enc) {
-        str = read_input_message(&str_len); // read second row
+    int capacity;
+    scanf("%d", &capacity);
+    /*str_enc doesnt read ---> PROBLEM*/
+    str_enc = read_input_message(&str_enc_len, &capacity); // read first row 
+    if (str_enc)
+    {
+        str = read_input_message(&str_len, &capacity); // read second row
     }
-    if (str_enc == NULL || str == NULL) {
+    
+    /*just for me*/
+    printf("%d %d\n", str_enc_len, str_len);
+    printf("%s\n", str);
+
+    if (str_enc == NULL || str == NULL)
+    {
         ret = ERROR_INPUT; // exit if input is char from [a-z_A-Z]
-    } else if (str_enc_len != str_len) {
+    }
+    else if (str_enc_len != str_len)
+    {
         ret = ERROR_LENGHT; // exit if first and second row doesn't have same
                             // lenght
-    } else {
+    }
+    else
+    {
         compare(str, str_enc, str_enc_len); // start comparing
     }
     print_error(ret);
@@ -42,35 +58,42 @@ int main(int argc, char *argv[])
     return ret;
 }
 
-char *read_input_message(int *str_len)
-{
-    int capacity = INT_STR_LEN;
+char *read_input_message(int *str_len, int *capacity)
+{   
+    //printf("%d\n", *capacity);
     int len = 0;
-    char *str = malloc(capacity * sizeof(char));
-    if (str == NULL) {
+    char *str = malloc(*capacity * sizeof(char));
+    if (str == NULL)
+    {
         fprintf(stderr, " ERROR MALLOC\n"); // improbable
         exit(-1);
-    } else {
+    }
+    else
+    {
         int a;
-        while ((a = getchar()) != EOF && a != '\n') {
+        while ((a = getchar()) != EOF && a != '\n')
+        {
             if (!((a >= 'a' && a <= 'z') ||
-                  (a >= 'A' && a <= 'Z'))) { // input out of range
+                  (a >= 'A' && a <= 'Z')))
+            { // input out of range
                 free(str);
                 str = NULL;
                 len = 0;
                 break;
             }
 
-            if (capacity == len) { // reallocate
-                char *tmp = realloc(str, capacity * 2);
-                if (tmp == NULL) {
+            if (*capacity == len)
+            { // reallocate
+                char *tmp = realloc(str, *capacity * 2);
+                if (tmp == NULL)
+                {
                     fprintf(stderr, "ERROR REALLOC\n");
                     free(str);
                     str = NULL;
                     len = 0;
                     break;
                 }
-                capacity *= 2;
+                *capacity *= 2;
                 str = tmp;
             }
             str[len++] = a;
@@ -86,7 +109,8 @@ void rotate(char original[], int len_org)
     // it always rotate whole string plus one in ASCII table
 
     char c;
-    for (int i = 0; i < len_org; ++i) {
+    for (int i = 0; i < len_org; ++i)
+    {
         c = original[i];
         if (c == 0)
             break;
@@ -107,30 +131,38 @@ void compare(char str[], char str_enc[], int str_enc_len)
     int match = 0; // number of matches with input array
     char *best_match = malloc((str_enc_len + 1) * sizeof(char));
     char *str_tmp = malloc((str_enc_len + 1) * sizeof(char));
-    if ((str_tmp == NULL) || (best_match == NULL)) {
+    if ((str_tmp == NULL) || (best_match == NULL))
+    {
         fprintf(stderr, "ERROR MALLOC\n");
         exit(-1);
     }
     best_match[str_enc_len] = 0;
     str_tmp[str_enc_len] = 0;
-    for (int i = 0; i < str_enc_len; ++i) {
+    for (int i = 0; i < str_enc_len; ++i)
+    {
         str_tmp[i] = str_enc[i];
     }
-    for (int i = 0; i < 57; ++i) {
-        for (int j = 0; j < str_enc_len; ++j) {
+    for (int i = 0; i < 57; ++i)
+    {
+        for (int j = 0; j < str_enc_len; ++j)
+        {
             if (str_tmp[j] == str[j])
                 counter++; // counting letter matches with input array
         }
-        if (match < counter) {
+        if (match < counter)
+        {
             match = counter;
             counter = 0;
             memset(best_match, 0, str_enc_len); // everytime it find bigger
                                                 // match clear temporary array
-            for (int k = 0; k < str_enc_len; ++k) {
+            for (int k = 0; k < str_enc_len; ++k)
+            {
                 best_match[k] =
                     str_tmp[k]; // then write in word with more matches
             }
-        } else {
+        }
+        else
+        {
             counter = 0;
         }
         rotate(str_tmp, str_enc_len); // call rotate function
@@ -142,7 +174,8 @@ void compare(char str[], char str_enc[], int str_enc_len)
 
 void print_error(int error)
 {
-    switch (error) {
+    switch (error)
+    {
     case ERROR_INPUT:
         fprintf(stderr, "%s\n", error_str_input);
         break;
