72d85c0ca5c472a089b9f46931ea56ecf605fcae
pantom <tomas.bahnik@gmail.com>
2022-11-21 23:40:31 +0100

    add reading matrix into array

--- my_codes_working/main.c
+++ my_codes_working/main.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    int* arr_matrixs[100];
+int main(void) {
+    int *arr_matrixs[100];
     char operators[100];
     int numb, rows, cols;
     int counter = 0;
     scanf("%d %d", &rows, &cols);
     int matrix[rows][cols];
 
-    for (int r = 0; r < rows; ++r)
-    {
-        for (int c = 0; c < cols; ++c)
-        {
+
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
             scanf("%d", &(matrix[r][c]));
         }
     }
-    for(int i = 0; i < rows; i++){
-        for (int j =0; j < cols; j++){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
-    arr_matrixs[0] = *matrix;
-    printf("\n");
-    printf("%d ", *arr_matrixs[0]);
 
+   arr_matrixs[0] = (int *) matrix;
+   printf("\n");
 
-
-
-
-
-    /* printf("%d %d\n", rows, cols);
-    for (int r = 0; r < rows; ++r)
-    {
-        for (int c = 0; c < cols; ++c)
-        {
-            printf("%d ", matrix[r][c]);
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", *(arr_matrixs[0] + i + j + count));
         }
+        count += 3;
         printf("\n");
-    } */
+    }
+
     return 0;
 }
