1a67c7fb0254be37ad15f36d8bd5f90f38f10e3d
pantom <tomas.bahnik@gmail.com>
2022-11-28 00:27:25 +0100

    nejde

--- hw08/main.c
+++ hw08/main.c
@@ -5,43 +5,42 @@
 #define ERROR_MALLOC 101
 #define ERROR_REALLOC 102
 
-int main(int argc, char *argv[])
-{
-  int capacity = LENGTH;
-  int load = 0;
-  int count = 0;
+int main(int argc, char *argv[]) {
+    int capacity = LENGTH;
+    int load = 0;
+    int count = 0;
 
-  // allocate memory for 10 numbers
-  int *arr_line = malloc(sizeof(int) * capacity);
+    // allocate memory for 10 numbers
+    int *arr_line = malloc(sizeof(int) * capacity);
 
-  if (arr_line == NULL) /*error malloc, for sure*/
-  {
-    fprintf(stderr, "Error malloc\n");
-    return ERROR_MALLOC;
-  }
-  // malloc ok ---> continue scanning numbers
-  else
-  {
-    while (scanf("%d", &arr_line[load]) == 1)
+    if (arr_line == NULL) /*error malloc, for sure*/
     {
-      count++;
-      if (load == capacity) /*array is full*/
-      {
-        capacity *= 2; /*double size array*/
-        int *tmp = realloc(arr_line, capacity);
-        if (tmp == NULL) /*error realloc, for sure*/
-        {
-          fprintf(stderr, "Error realloc\n");
-          free(arr_line);
-          arr_line = NULL;
-          return ERROR_REALLOC;
-        }
-        arr_line = tmp;
-      }
-      load++;
+        fprintf(stderr, "Error malloc\n");
+        return ERROR_MALLOC;
     }
-  }
-  free(arr_line);
-  printf("%d", *arr_line);
-  return 0;
+        // malloc ok ---> continue scanning numbers
+    else {
+        while (scanf("%d", &arr_line[load]) != EOF) {
+            count++;
+            if (load == capacity) /*array is full*/
+            {
+                int *tmp = realloc(arr_line, capacity * 2);
+                if (tmp == NULL) /*error realloc, for sure*/
+                {
+                    fprintf(stderr, "Error realloc\n");
+                    free(arr_line);
+                    arr_line = NULL;
+                    return ERROR_REALLOC;
+                }
+                arr_line = tmp;
+                capacity *= 2; /*double size array*/
+            }
+            load++;
+        }
+    }
+    for (int i = 0; i < capacity; i++) {
+        printf("%d ", arr_line[i]);
+    }
+    free(arr_line);
+    return 0;
 }
