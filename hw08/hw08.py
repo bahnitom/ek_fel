d18442e9bf4fbf30c4e06cdbafe606aa00196582
Tomas Bahnik <tomas.bahnik@email.cz>
2022-12-11 22:37:29 +0100

    rename vars, resolve error, format output

--- hw08/hw08.py
+++ hw08/hw08.py
@@ -9,35 +9,44 @@ def load_numbers(file) -> List[int]:
     with open(file, 'r') as input_file:
         for line in input_file:
             # one line only
-            return list(map(int, line.split()))
+            try:
+                return list(map(int, line.split()))
+            except ValueError as err:
+                print("Invalid input")
+                sys.exit(100)
 
 
-def fill_bins(input_data: List[int]):
-    n_intervals = input_data[0]
-    numbers = input_data[1:]
-    bins = init_bins(n_bins=input_data[0])
-    min_num = min(input_data[1:])
-    interval_size = (max(numbers) - min(numbers)) / n_intervals
-    for x_i in numbers:
-        bin_number = put_in_bin(x=x_i, min_num=min_num, interval_size=interval_size)
-        # increase count of items in bin with bin_number
+def fill_bins(data: List[int]):
+    n_intervals = data[0]
+    series = data[1:]
+    bins = init_bins(n_bins=n_intervals)
+    min_num = min(series)
+    interval_size = (max(series) - min(series)) / n_intervals
+    for x in series:
+        bin_number = put_in_bin(x=x, min_num=min_num, interval_size=interval_size)
+        # increase count of items in the bin with bin_number
         bins[bin_number] += 1
-    return bins, interval_size
+    return bins
 
 
-def print_histogram(bins: List[int], interval_size: float, min_number: int):
+def print_histogram(bins: List[int], data: List[int]):
     """
     Scale and print bins. bins is array of int.
     The values at index i is the number of item in the bin i
     """
     bin_max = max(bins)
     scale_by = 25 / bin_max
+    n_intervals = data[0]
+    series = data[1:]
+    min_number = min(series)
+    interval_size = (max(series) - min(series)) / n_intervals
     for bin_number in range(len(bins)):
         n_of_signs = math.floor(scale_by * bins[bin_number])
         lower_bound = round(min_number + bin_number * interval_size, ndigits=1)
         upper_bound = round(min_number + (bin_number + 1) * interval_size, ndigits=1)
-        print("bin %2d, (%1d) %5.1f - %5.1f |%s" %
-              (bin_number, bins[bin_number], upper_bound, lower_bound, '=' * n_of_signs))
+        print("%5.1f - %5.1f |%s" % (lower_bound, upper_bound, '=' * n_of_signs))
+        # print("bin %2d, (%1d) %5.1f - %5.1f |%s" %
+        #       (bin_number, bins[bin_number], lower_bound, upper_bound, '=' * n_of_signs))
         # print(f"bin {bin_number} ({bins[bin_number]}) {lower_bound} - {upper_bound}|{'=' * n_of_signs}")
 
 
@@ -46,24 +55,26 @@ def init_bins(n_bins: int) -> List[int]:
     return [0] * n_bins
 
 
-eps = 0.001
+# small number
+EPSILON = 0.001
 
 
 def put_in_bin(x: int, min_num: int, interval_size: float) -> int:
     """
+    Main logic here!
     Number of the bin for given x. Bins are numbered from <0, number of interval), i.e
     there number of bins = number of interval
     """
     # min values belong to bin 0
     # eps values means that the interval is right closed
-    return 0 if x == min_num else math.floor((x - eps - min_num) / interval_size)
+    return 0 if x == min_num else math.floor((x - EPSILON - min_num) / interval_size)
 
 
-def print_header(input_data: List[int]):
+def print_header(data: List[int]):
     # first numer is number of intervals
-    n_intervals = input_data[0]
+    n_intervals = data[0]
     # remaking numeraires is the series
-    numbers = input_data[1:]
+    numbers = data[1:]
     median_num = statistics.median(numbers)
     print(f"Median {median_num}")
     print(f"Pocet cisel: {len(numbers)}")
@@ -79,9 +90,7 @@ def print_header(input_data: List[int]):
 if __name__ == "__main__":
     f = Path(sys.argv[1])
     input_data = load_numbers(f)
-    print_header(input_data=input_data)
-    filled_bins, interval_size = fill_bins(input_data=input_data)
-    numbers = input_data[1:]
-    min_number = min(numbers)
+    print_header(data=input_data)
+    filled_bins = fill_bins(data=input_data)
     print(f"Histogram:")
-    print_histogram(filled_bins, interval_size=interval_size, min_number=min_number)
+    print_histogram(filled_bins, data=input_data)
