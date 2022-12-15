8894d6d024386c5837ae91e77a974e4c810e581e
pantom <tomas.bahnik@gmail.com>
2022-12-15 14:17:31 +0100

    dynamically reading input

--- hw09/main.c
+++ hw09/main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* The main program */
-int main(int argc, char *argv[])
-{
-  // TODO - insert your code here
-  
-  return 0;
+#define SIZE 20
+enum {
+    ERROR_MALLOC = -1, ERROR_REALLOC = -2
+};
+
+
+int main(void) {
+    size_t capacity = SIZE;
+    char *input = malloc(capacity * sizeof(char));
+    if (input == NULL) {
+        fprintf(stderr, "Error malloc\n");
+        exit(ERROR_MALLOC);
+    } else {
+        fgets(input, (int) capacity, stdin);
+        while (capacity - 1 == strlen(input) && input[capacity - 2] != '\n') {
+            char *tmp = realloc(input, 2 * capacity * sizeof(char));
+            if (tmp == NULL) {
+                fprintf(stderr, "Error realloc\n");
+                free(input);
+                input = NULL;
+                exit(ERROR_REALLOC);
+            }
+            input = tmp;
+            // read missing part of sentence
+            fgets(input + capacity - 1, (int) capacity + 1, stdin); //TODOs
+            capacity *= 2;
+        }
+    }
+    printf("%lu\n", strlen(input));
+    printf("%s", input);
+    return 0;
 }
-
