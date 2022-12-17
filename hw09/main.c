ce59f1310f02f2680ac1b18c2642caa95306d112
pantom <tomas.bahnik@gmail.com>
2022-12-17 20:29:53 +0100

    add to end of string '\0' + working version of removing punctuation symbols

--- hw09/main.c
+++ hw09/main.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define SIZE 20
 enum {
     ERROR_MALLOC = -1, ERROR_REALLOC = -2
 };
 
+/* functions */
+char *load_input();
 
 int main(void) {
+
+    char *input = NULL;
+    input = load_input();
+
+    for(int i = 0; i < strlen(input); i ++){
+        if (ispunct(input[i])){
+            // copy rest of string behind punctuation symbol and paste it in actual position
+            strcpy(input + i, input + i + 1);
+            // check again same symbol, because of possible existence punctuation
+            i --;
+        }
+    }
+
+    printf("%lu\n", strlen(input));
+    printf("%s\n", input);
+    return 0;
+}
+
+char *load_input() {
     size_t capacity = SIZE;
     char *input = malloc(capacity * sizeof(char));
     if (input == NULL) {
@@ -26,11 +48,11 @@ int main(void) {
             }
             input = tmp;
             // read missing part of sentence
-            fgets(input + capacity - 1, (int) capacity + 1, stdin); //TODOs
+            fgets(input + capacity - 1, (int) capacity + 1, stdin);
             capacity *= 2;
         }
     }
-    printf("%lu\n", strlen(input));
-    printf("%s", input);
-    return 0;
+    // Add null character at the end of the string
+    input[strlen(input)] = '\0';
+    return input;
 }
