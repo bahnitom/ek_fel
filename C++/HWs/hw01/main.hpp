999c4b084a9b3b456f800a527bee0f52adbbd0d9
Tomas Bahnik <tomas.bahnik@email.cz>
2023-03-22 09:02:01 +0100

    remove obsolete (1x used constants)

--- C++/HWs/hw01/main.hpp
+++ C++/HWs/hw01/main.hpp
@@ -4,21 +4,6 @@
 
 #include "parse.hpp"
 
-static const int OUT_OFF_RANGE_ERROR = 100;
-
-static const int INVALID_INPUT_ERROR = 101;
-
-static const int INVALID_CONFIG_ERROR = 102;
-
-static const int CELL_IS_TOO_SHORT_ERROR = 103;
-
-static const char *const OUT_OFF_RANGE_MSG = "Out of range\n";
-
-static const char *const INVALID_CONFIG_MSG = "Invalid configuration\n";
-
-static const char *const INVALID_INPUT_MSG = "Invalid input\n";
-
-static const char *const CELL_IS_TOO_SHORT_MSG = "Cell is too short\n";
 
 int checkTableRows(const cfg_values_t &cfgValues, const std::vector<std::vector<int>> &rows);
 
@@ -33,9 +18,9 @@ typedef struct {
     std::string message;
 } error_with_msg_t;
 
-static const error_with_msg_t OUT_OFF_RANGE = {OUT_OFF_RANGE_ERROR, OUT_OFF_RANGE_MSG};
-static const error_with_msg_t INVALID_INPUT = {INVALID_INPUT_ERROR, INVALID_INPUT_MSG};
-static const error_with_msg_t CELL_IS_TOO_SHORT = {CELL_IS_TOO_SHORT_ERROR, CELL_IS_TOO_SHORT_MSG};
-static const error_with_msg_t INVALID_CONFIG = {INVALID_CONFIG_ERROR, INVALID_CONFIG_MSG};
+static const error_with_msg_t OUT_OFF_RANGE = {100, "Out of range\n"};
+static const error_with_msg_t INVALID_INPUT = {101, "Invalid input\n"};
+static const error_with_msg_t INVALID_CONFIG = {102, "Invalid configuration\n"};
+static const error_with_msg_t CELL_IS_TOO_SHORT = {103, "Cell is too short\n"};
 
 #endif
